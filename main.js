daee5e1df676d15090df7ffe293b3522b4260416
LeonardoSarallo <leo.sarallo@gmail.com>
2018-12-04 19:39:27 +0100

    gestitotastoinviook

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  //Gestisco il click
+
   $('#invio').click(function() {
 
    var text = $('#textval').val();
@@ -20,6 +22,32 @@ $(document).ready(function() {
     },1000);
 
   });
+
+  // Gestisco da tastiera con tasto invio
+
+
+  $('#textval').keypress(function(e) {
+    if (e.which == 13)
+    {
+      var text = $('#textval').val();
+      var messaggioClonato = $('.template .chat').clone();
+      var clearTemplate = $('.template .clear1').clone();
+      messaggioClonato.text(text);
+      $('.textdisplay').append(messaggioClonato);
+      $('.textdisplay').append(clearTemplate);
+      $('#textval').val('');
+
+      setTimeout(function() {
+        var rispostaClonata = $('.template .answer').clone();
+        var clearTemplate = $('.template .clear1').clone();
+        rispostaClonata.text('ok');
+        $('.textdisplay').append(rispostaClonata);
+        $('.textdisplay').append(clearTemplate);
+
+       },1000);
+    }
+  });
+
 
   $('#search').click(function() {
     var nameSearch = $('#searchinput').val();
